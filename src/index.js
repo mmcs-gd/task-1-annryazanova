74ad66ab18361eae69f7ea825e20935cec5342ff
aryazanova <aryazanova@gamenuts.ru>
2019-09-24 23:46:17 +0300

    Add simple bonus

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ const directionType = {
 	up: 2,
 	left: 3,
 	right: 4,
-}
+};
 
 function onMouseMove(e) {
     gameState.pointer.x = e.pageX;
-    gameState.pointer.y = e.pageY
+    gameState.pointer.y = e.pageY;
 }
 
 function queueUpdates(numTicks) {
@@ -28,9 +28,11 @@ function draw(tFrame) {
     // clear canvas
     context.clearRect(0, 0, canvas.width, canvas.height);
 
-    drawPlatform(context)
-    drawBall(context)
-    drawscore(context)
+    drawPlatform(context);
+    drawBall(context);
+    drawScore(context);
+	drawBonus(context);
+	//drawDebugInfo(context);
 }
 
 function update(tick) {
@@ -52,14 +54,32 @@ function update(tick) {
 	
 	// update ball speed every 30 seconds
 	if (gameState.lastTick - ball.lastSpeedIncrease > 30000) {
-		ball.vx += ball.vx * .1;
-		ball.vy += ball.vy * .1;
+		ball.vx *= 1.1;
+		ball.vy *= 1.1;
 		ball.lastSpeedIncrease = gameState.lastTick;
 	}
 	
-	checkPlayerCollision(ball);
-	checkTopCollision(ball);
+    const bonus = gameState.bonus
+	
+	// create bonus in random place every 15 seconds
+	if (gameState.lastTick - bonus.lastInstance > 15000) {
+		bonus.x = getRandomIntInclusive(0, canvas.width); 
+		bonus.y = getRandomIntInclusive(0, canvas.height / 2); 
+		bonus.vx = getRandomIntInclusive(-5, 5);
+		bonus.vy = getRandomIntInclusive(1, 5);
+		bonus.radius = getRandomIntInclusive(10, 20);
+		bonus.width = getRandomIntInclusive(5, 10);
+		bonus.isCreated = true; 
+		bonus.lastInstance = gameState.lastTick;
+	}
+	
     checkGameEnd();
+	checkPlayerCollision(ball, function() {
+		ball.vy *= (-1);
+		ball.direction = directionType.up;
+	});
+	checkTopCollision(ball);
+	checkBonusState();
 }
 
 function run(tFrame) {
@@ -99,7 +119,7 @@ function drawBall(context) {
     context.closePath();
 }
 
-function drawscore(context) {
+function drawScore(context) {
     const {x, y, width, height, value} = gameState.score;
     context.beginPath();
     context.rect(x, y, width, height);
@@ -108,8 +128,27 @@ function drawscore(context) {
     context.fillStyle = "#FFFFFF";
     context.textAlign = "center";
     context.font = "18px serif";
-    context.fillText(value, x + width / 2, y + height / 2 + 5); 
+    context.fillText("score: " + value, x + width / 2, y + height / 2 + 5); 
     context.closePath();
+}
+
+function drawBonus(context) {
+	if (gameState.bonus.isCreated) {
+		const {x, y, radius, width} = gameState.bonus;
+		context.beginPath();
+		context.rect(x - radius, y - width / 2, 2 * radius, width);
+		context.rect(x - width / 2, y - radius, width, 2 * radius);
+		context.fillStyle = "#FFFF00";
+		context.fill();
+		context.closePath();		
+	}
+}
+
+function drawDebugInfo(context) {
+    context.fillStyle = "#000000";
+    context.textAlign = "left";
+    context.font = "18px serif";
+    context.fillText("vx: " + gameState.bonus.vx + ", vy: " + gameState.bonus.vy + ", y: " + gameState.bonus.y, 5, 60);	
 }
 
 function setup() {
@@ -144,35 +183,44 @@ function setup() {
         vx: 0,
         vy: 5,
 		direction: directionType.down,
-		lastSpeedIncrease : 0,
+		lastSpeedIncrease: 0,
     };
     gameState.score = {
-        x : 5,
-        y : 5,
-        width : 100,
-        height : 36,
-        value : 0,
+        x: 5,
+        y: 5,
+        width: 100,
+        height: 36,
+        value: 0,
 		lastSave: 0,
-    }
+    };
+	gameState.bonus = {
+		x: 0,
+		y: 0, 
+		radius: 0,
+		width: 0,
+		vx: 0,
+		vy: 0,
+		lastInstance: 0,
+		isCreated: false,
+	}
 }
 
 // check bottom collision and finish game
 function checkGameEnd() {
-    const ball = gameState.ball
-    if (ball.y >= canvas.height + ball.radius) {
-        stopGame(gameState.stopCycle);
-    }
+    const ball = gameState.ball;
+	checkBottomCollision(ball, function() {
+		stopGame(gameState.stopCycle)
+	});
 }
 
-function checkPlayerCollision(ball){
+function checkPlayerCollision(obj, callback){
 	player = gameState.player;
 	leftPlatformPart = player.x - player.width / 2;
 	rightPlatformPart = player.x + player.width / 2
-	if (ball.y + ball.radius >= player.y && 
-	ball.x > leftPlatformPart && 
-	ball.x < rightPlatformPart) {
-		ball.vy *= (-1);
-		ball.direction = directionType.up;
+	if (obj.y + obj.radius >= player.y && 
+	obj.x > leftPlatformPart && 
+	obj.x < rightPlatformPart) {
+		callback();
 	}
 }
 
@@ -182,6 +230,40 @@ function checkTopCollision(ball){
 		ball.vy *= (-1);
 		ball.direction = directionType.down;
 	}
+}
+
+function checkBottomCollision(obj, callback){
+	if (obj.y >= canvas.height + obj.radius) {
+        callback();		
+    }
+}
+
+// Getting a random integer in a given interval, inclusive
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; 
+}
+
+function checkBonusState(){
+	if (gameState.bonus.isCreated) {
+		bonus = gameState.bonus;
+		updateBonusPosition(bonus);
+	}
+}
+
+function updateBonusPosition(bonus){
+	bonus.y += bonus.vy;
+	bonus.x += bonus.vx; // TO DO: исправить выход за границы экрана -> отскок от левой и правой стен
+	
+	checkPlayerCollision(bonus, function() {
+		gameState.score.value += 15;
+		bonus.isCreated = false;
+	});
+	
+	checkBottomCollision(bonus, function() {
+		gameState.bonus.isCreated = false;
+	});
 }
 
 setup();
